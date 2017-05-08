5295555fc17b25bee5c1b8791afe2dc2d532d0e9
PredavacILI01 <predavacILI01@ucione.local>
2017-05-08 19:37:50 +0200

    Add test files

--- config/database.php
+++ config/database.php
@@ -78,6 +78,17 @@ return [
             'schema' => 'public',
             'sslmode' => 'prefer',
         ],
+		'testing' => [
+            'driver' => 'mysql',
+            'host' => env('DB_TEST_HOST', 'localhost'),
+            'database' => env('DB_TEST_DATABASE', 'algebra_box'),
+            'username' => env('DB_TEST_USERNAME', 'root'),
+            'password' => env('DB_TEST_PASSWORD', ''),
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+            'strict' => false,
+        ],
 
     ],
 
